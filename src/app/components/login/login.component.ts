c4ca34fe152fc9cb35da47042baa509782d87f42
HimanshuW07 <97220341+HimanshuW07@users.noreply.github.com>
2025-02-04 23:13:21 +0530

    Update login.component.ts
    Added login pop-up

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -4,11 +4,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { AuthService } from '../../services/auth.service';
 import { Router, RouterLink } from '@angular/router';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatInputModule, MatButtonModule, ReactiveFormsModule, RouterLink],
+  imports: [MatInputModule, MatButtonModule, ReactiveFormsModule, RouterLink, MatSnackBarModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -22,11 +23,18 @@ export class LoginComponent {
 
     authService = inject(AuthService)
     router = inject(Router);
+    snackBar = inject(MatSnackBar);
     
     login() {
       this.authService.login(this.loginForm.value.email!,this.loginForm.value.password!).subscribe((result : any)=> {
         localStorage.setItem('token', result.token);
         localStorage.setItem('user', JSON.stringify(result.user));
+
+        this.snackBar.open('Login Successful', '', {
+          duration: 2000,
+          panelClass: ['bg-green-500', 'text-white'],
+        });
+
         this.router.navigateByUrl("/");
       })
     }
